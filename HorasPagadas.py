59cf9e1c6eabdf9d90c387928a5b9c68c2c712de
bryanlolry <50712646+bryanlolry@users.noreply.github.com>
2020-04-23 00:34:21 +0200

    Update HorasPagadas.py

--- HorasPagadas.py
+++ HorasPagadas.py
@@ -1,3 +1,4 @@
+#Escribir un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.
 while(True): #bucle para repetir indefinidamente
 	horas = int(input("Ingrese el numero de horas trabajadas: ")) #entrada para obtener los datos de horas 
 	pago = int(input("Ingrese el salario por hora trabajada: ")) #entrada para obtener los datos de pago por hora
